236c56d70158ef9372c970efb9e9cfd622abee69
Александр Донченко <darkprince3738@gmail.com>
2021-06-13 00:35:10 +0300

    All Tasks

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int count = (new DateTime(DateTime.Now.Year, month, day) - DateTime.Now).Days;
+            if (count < 0)
+            {
+                Console.WriteLine($"Left until the birthday {365+count}");
+            }
+            else
+            {
+                Console.WriteLine($"Left until the birthday {count}");
+            }
+        }
+    }
+}
---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsolePrinter.Print("Red flower", ConsolePrinter.OutputColors.Red);
+            ConsolePrinter.Print("Green flower", ConsolePrinter.OutputColors.Green);
+            ConsolePrinter.Print("Blue flower", ConsolePrinter.OutputColors.Blue);
+            ConsolePrinter.Print("Black flower", ConsolePrinter.OutputColors.Black);
+            ConsolePrinter.Print("Yellow flower", ConsolePrinter.OutputColors.Yellow);
+            ConsolePrinter.Print("White flower", ConsolePrinter.OutputColors.White);
+
+            Console.ReadKey(true);
+        }
+    }
+    static class ConsolePrinter
+    {
+        public enum OutputColors 
+        { 
+            
+            Red = 4, 
+            Green = 2, 
+            Blue = 1,
+            Black = 0,
+            Yellow = 6,
+            White = 7
+        }
+
+        public static void Print(string text, OutputColors outputColor)
+        {
+            Console.ForegroundColor = (ConsoleColor)Convert.ToInt32(outputColor);
+            Console.WriteLine(text);
+        }
+    }
+}
